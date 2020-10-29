6987655487bbbac1666ede38d3a6ae1ff4acea03
Christian Dalton <cdalton713@gmail.com>
2020-10-29 17:52:48 -0500

    added about page, header navbar.

--- frontend/src/components/app/routes.js
+++ frontend/src/components/app/routes.js
@@ -1,18 +1,23 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import routes from "../../utils/constants/routes.json"
 import {LoginSelect} from "../../routes/login/login";
 import {StudentRoute} from "../../routes/student/student";
 import {AdminRoute} from "../../routes/admin/admin";
+import {AboutRoute} from "../../routes/about/about";
+import {NavBar} from "../navbar/navbar";
 
 
 export const GlobalRouter = (props) => {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path={routes.LOGIN} component={LoginSelect} />
-                <Route path={routes.STUDENT} component={StudentRoute} />
-                <Route path={routes.ADMIN} component={AdminRoute} />
-            </Switch>
-        </Router>
+    return (<>
+
+            <Router>
+                <Switch>
+                    <Route exact path={routes.LOGIN} component={LoginSelect}/>
+                    <Route path={routes.STUDENT} component={StudentRoute}/>
+                    <Route path={routes.ADMIN} component={AdminRoute}/>
+                    <Route path={routes.ABOUT} component={AboutRoute}/>
+                </Switch>
+            </Router></>
     );
 };
--- frontend/src/routes/student/student.js
+++ frontend/src/routes/student/student.js
@@ -11,27 +11,31 @@ import {
     EuiPageHeaderSection,
     EuiTitle,
 } from '@elastic/eui';
+import {NavBar} from "../../components/navbar/navbar";
 
-export const StudentRoute = () => (
-    <EuiPage>
-        <EuiPageBody component="div">
-            <EuiPageHeader>
-                <EuiPageHeaderSection>
-                    <EuiTitle size="l">
-                        <h1>Student Support</h1>
-                    </EuiTitle>
-                </EuiPageHeaderSection>
-            </EuiPageHeader>
-            <EuiPageContent>
-                <EuiPageContentHeader>
-                    <EuiPageContentHeaderSection>
-                        <EuiTitle>
-                            <h2>Submit Ticket</h2>
+export const StudentRoute = (props) => (
+    <>
+        <NavBar location={props.location}/>
+        <EuiPage>
+            <EuiPageBody component="div">
+                <EuiPageHeader>
+                    <EuiPageHeaderSection>
+                        <EuiTitle size="l">
+                            <h1>Student Support</h1>
                         </EuiTitle>
-                    </EuiPageContentHeaderSection>
-                </EuiPageContentHeader>
-                <EuiPageContentBody>Content body</EuiPageContentBody>
-            </EuiPageContent>
-        </EuiPageBody>
-    </EuiPage>
+                    </EuiPageHeaderSection>
+                </EuiPageHeader>
+                <EuiPageContent>
+                    <EuiPageContentHeader>
+                        <EuiPageContentHeaderSection>
+                            <EuiTitle>
+                                <h2>Submit Ticket</h2>
+                            </EuiTitle>
+                        </EuiPageContentHeaderSection>
+                    </EuiPageContentHeader>
+                    <EuiPageContentBody>Content body</EuiPageContentBody>
+                </EuiPageContent>
+            </EuiPageBody>
+        </EuiPage>
+    </>
 );
