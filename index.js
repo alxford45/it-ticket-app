5c7a9a972786a3a2f90fca51f0ba429727953838
niosver <raladnier@hotmail.com>
2020-11-14 13:43:29 -0600

    updated for heroku building

--- index.js
+++ index.js
@@ -5,23 +5,38 @@ var port = process.env.PORT;
 if(port === undefined) {
     port = 5000;
 }
-/*
-const client = new Client({
-    connectionString: process.env.DATABSE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
+var options = process.env.DATABSE_URL;
+var client = undefined;
+if(options !== undefined) {
+    client = new Client({
+        connectionString: options,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+}
+else {
+    client = new Client({
+        user: 'brvuirrqqcjzsb',
+        host: 'ec2-54-158-190-214.compute-1.amazonaws.com',
+        database: 'd3t7i9dt3u6h4g',
+        password: '37f36b1e7cb6167c12f84c9e356c14535824e0a6c7e0dfe5fb71417f2be8d5b8',
+        port: '5432',
+        ssl: {
+            rejectUnauthorized:false
+        }
+    });
+}
 
 client.connect();
-*/
+
 
 var app = express();
 
-app.use(express.static(__dirname + "/build"));
+app.use(express.static(__dirname + "/client/build"));
 
 app.get('/*', function (req,res) {
-    res.sendFile(__dirname + "/build/index.html");
+    res.sendFile(__dirname + "/client/build/index.html");
 });
 
 console.log(`Listening on ${port}`);
