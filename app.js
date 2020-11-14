c6f1d18cfd5a076b2986d467c1e24c15fa634901
niosver <raladnier@hotmail.com>
2020-11-14 10:48:02 -0600

    added route for react handling

--- app.js
+++ app.js
@@ -15,5 +15,9 @@ var app = express();
 
 app.use(express.static(__dirname + "/build"));
 
+app.get('/*', function (req,res) {
+    res.sendFile(__dirname + "/build/index.html");
+});
+
 console.log("Listening on 8888");
 app.listen(8888);
