2da8bc2a66a2544b94a2cb8a19032180ef48d7be
Christian Dalton <cdalton713@gmail.com>
2020-11-15 22:46:57 -0600

    started admin view

---
+++ client/src/components/form/MyTextField.js
@@ -0,0 +1,26 @@
+import { EuiFieldText, EuiFormRow } from "@elastic/eui";
+import React from "react";
+import { errorMessages } from "./fields";
+
+var _ = require("lodash");
+export const MyTextField = (
+  { name, data, handleChange, handleBlur },
+  ...props
+) => {
+  const item = _.find(data, ["name", name]);
+  return (
+    <EuiFormRow
+      label={item.label}
+      error={[
+        _.find(errorMessages, ["error_type", item.error_type]).error_message,
+      ]}
+      isInvalid={item.error}
+    >
+      <EuiFieldText
+        name={item.name}
+        onChange={(e) => handleChange(e)}
+        onBlur={(e) => handleBlur(e)}
+      />
+    </EuiFormRow>
+  );
+};
---
+++ client/src/components/form/adminView.js
@@ -0,0 +1,168 @@
+import {
+  EuiButton,
+  EuiCode,
+  EuiFlexGrid,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiFormRow,
+  EuiSpacer,
+  EuiTextArea,
+  EuiTitle,
+} from "@elastic/eui";
+import React, { useState } from "react";
+import { MyTextField } from "./MyTextField";
+import { MySelectField } from "./MySelectField";
+import { addToast } from "../toast";
+import { DEBUG } from "../app/app";
+import { errorMessages, selectOptions } from "./fields";
+import { handleFormFieldBlur, handleFormFieldChange } from "./handlers";
+
+var _ = require("lodash");
+
+export const AdminView = ({ data, setData }, ...props) => {
+  return (
+    <>
+      <EuiSpacer />
+      <EuiTitle size={"s"}>
+        <h3>Technical Details</h3>
+      </EuiTitle>
+      <EuiFlexGroup style={{ maxWidth: 1000 }}>
+        <EuiFlexItem>
+          <MyTextField
+            name={"first_name"}
+            data={data}
+            handleChange={(e) => handleFormFieldChange(e, data, setData)}
+            handleBlur={(e) => handleFormFieldBlur(e, data, setData)}
+          />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <MyTextField
+            name={"last_name"}
+            data={data}
+            handleChange={(e) => handleFormFieldChange(e, data, setData)}
+            handleBlur={(e) => handleFormFieldBlur(e, data, setData)}
+          />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <MyTextField
+            name={"lsu_id"}
+            data={data}
+            handleChange={(e) => handleFormFieldChange(e, data, setData)}
+            handleBlur={(e) => handleFormFieldBlur(e, data, setData)}
+          />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <MyTextField
+            name={"department"}
+            data={data}
+            handleChange={(e) => handleFormFieldChange(e, data, setData)}
+            handleBlur={(e) => handleFormFieldBlur(e, data, setData)}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiFlexGroup style={{ maxWidth: 1000 }}>
+        <EuiFlexItem grow={false} style={{ width: 250 }}>
+          <MyTextField
+            name={"email"}
+            data={data}
+            handleChange={(e) => handleFormFieldChange(e, data, setData)}
+            handleBlur={(e) => handleFormFieldBlur(e, data, setData)}
+          />
+        </EuiFlexItem>
+        <EuiFlexItem grow={false} style={{ width: 200 }}>
+          <MyTextField
+            name={"phone_number"}
+            data={data}
+            handleChange={(e) => handleFormFieldChange(e, data, setData)}
+            handleBlur={(e) => handleFormFieldBlur(e, data, setData)}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiSpacer />
+      <EuiTitle size={"s"}>
+        <h3>General Information</h3>
+      </EuiTitle>
+      <EuiFlexGroup style={{ maxWidth: 1000 }}>
+        <EuiFlexItem grow={false} style={{ width: 150 }}>
+          <MySelectField
+            name={"priority"}
+            data={data}
+            selectOptions={selectOptions}
+            handleChange={(e) => handleFormFieldChange(e, data, setData)}
+            handleBlur={(e) => handleFormFieldBlur(e, data, setData)}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiFlexGroup style={{ maxWidth: 1000 }}>
+        <EuiFlexItem>
+          <MyTextField
+            name={"manufacturer"}
+            data={data}
+            handleChange={(e) => handleFormFieldChange(e, data, setData)}
+            handleBlur={(e) => handleFormFieldBlur(e, data, setData)}
+          />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <MyTextField
+            name={"model"}
+            data={data}
+            handleChange={(e) => handleFormFieldChange(e, data, setData)}
+            handleBlur={(e) => handleFormFieldBlur(e, data, setData)}
+          />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <MyTextField
+            name={"operating_system"}
+            data={data}
+            handleChange={(e) => handleFormFieldChange(e, data, setData)}
+            handleBlur={(e) => handleFormFieldBlur(e, data, setData)}
+          />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <MyTextField
+            name={"operating_system_version"}
+            data={data}
+            handleChange={(e) => handleFormFieldChange(e, data, setData)}
+            handleBlur={(e) => handleFormFieldBlur(e, data, setData)}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiSpacer />
+      <EuiTitle size={"s"}>
+        <h3>Issue Information</h3>
+      </EuiTitle>
+      <EuiFlexGrid>
+        <EuiFlexItem>
+          <MySelectField
+            name={"problem_category"}
+            data={data}
+            handleChange={(e) => handleFormFieldChange(e, data, setData)}
+            selectOptions={selectOptions}
+            handleBlur={(e) => handleFormFieldBlur(e, data, setData)}
+          />
+        </EuiFlexItem>
+      </EuiFlexGrid>
+      <EuiFlexGroup style={{ maxWidth: 1000 }}>
+        <EuiFlexItem>
+          <EuiFormRow
+            label={_.find(data, ["name", "description"]).label}
+            error={[
+              _.find(errorMessages, [
+                "error_type",
+                _.find(data, ["name", "description"]).error_type,
+              ]).error_message,
+            ]}
+            isInvalid={_.find(data, ["name", "description"]).error}
+          >
+            <EuiTextArea
+              placeholder={"Computer crashes when..."}
+              name={_.find(data, ["name", "description"]).name}
+              onChange={(e) => handleFormFieldChange(e, data, setData)}
+              onBlur={(e) => handleFormFieldBlur(e, data, setData)}
+            />
+          </EuiFormRow>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </>
+  );
+};
