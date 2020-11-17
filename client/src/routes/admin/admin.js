0c6647bedf24994a1651414e0a9ed85af56a21d4
Christian Dalton <cdalton713@gmail.com>
2020-11-17 09:44:17 -0600

    re-added missing files

---
+++ client/src/routes/admin/admin.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+import {
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent,
+  EuiPageContentBody,
+  EuiPageContentHeader,
+  EuiPageContentHeaderSection,
+  EuiPageHeader,
+  EuiPageHeaderSection,
+  EuiTitle,
+  EuiFlexGroup,
+  EuiPanel,
+  EuiFlexItem,
+  EuiStat,
+  EuiIcon,
+  EuiSpacer,
+  EuiForm,
+  EuiButton,
+  EuiCode,
+} from "@elastic/eui";
+import { NavBar } from "../../components/navbar/navbar";
+import { Table } from "../../components/table/openTickets";
+import { AdminTicketFlyout } from "../../components/flyout/flyout";
+import { UserView } from "../../components/form/ManageTicketForm/userView";
+import { fields } from "../../components/form/ManageTicketForm/fields";
+import { handleFormSubmit } from "../../components/form/ManageTicketForm/handlers";
+import { AdminView } from "../../components/form/ManageTicketForm/adminView";
+import { SelectTechnician } from "./SelectTechnician";
+import { ManageTicket } from "./ManageTicket";
+
+export const AdminRoute = (props) => {
+  const [technician, setTechnician] = useState(false);
+  return (
+    <>
+      <NavBar
+        location={props.location}
+        technician={technician}
+        setTechnician={setTechnician}
+      />
+      <EuiPage>
+        <EuiPageBody component="div">
+          <div>
+            {technician === false ? (
+              <SelectTechnician setTechnician={setTechnician} />
+            ) : (
+              <ManageTicket />
+            )}
+          </div>
+        </EuiPageBody>
+      </EuiPage>
+    </>
+  );
+};
