3f1b8d3cab9487afbf1dd55ca49f5d08197d3fa3
Alex <alxford45@gmail.com>
2020-11-16 19:04:16 -0600

    backend now serves static files from client/build

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [],
+  imports: [ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'client/build'),
+      exclude: ['/api*','/auth']
+    }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule  {
+}
