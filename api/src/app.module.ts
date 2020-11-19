600bb9c81192223e1feea7e7a74d7bf7a556c2b3
Alex <alxford45@gmail.com>
2020-11-19 10:40:53 -0600

    nest generated resource ticket

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,13 +4,14 @@ import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TicketModule } from './ticket/ticket.module';
 
 
 @Module({
   imports: [ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'client/build'),
       exclude: ['/api*','/auth']
-    }), UserModule],
+    }), UserModule, TicketModule],
   controllers: [AppController],
   providers: [AppService],
 })
