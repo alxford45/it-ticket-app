30375e6882ee24db2c315b3d65bb66bc5f84ee33
Alex <alxford45@gmail.com>
2020-11-19 10:45:04 -0600

    added import to dbModule

--- api/src/ticket/ticket.module.ts
+++ api/src/ticket/ticket.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TicketService } from './ticket.service';
 import { TicketController } from './ticket.controller';
+import { DbModule } from 'src/db/db.module';
 
 @Module({
+  imports: [DbModule],
   controllers: [TicketController],
   providers: [TicketService],
 })
