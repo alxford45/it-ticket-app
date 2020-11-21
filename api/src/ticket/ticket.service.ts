cf82137de6bcaa9d9e418781911a056f619ee6b3
Alex <alxford45@gmail.com>
2020-11-20 18:02:26 -0600

    Implemented methods and response logging

--- api/src/ticket/ticket.service.ts
+++ api/src/ticket/ticket.service.ts
@@ -1,26 +1,155 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { Pool, QueryConfig } from 'pg';
+import { PG_CONNECTION } from 'src/connection';
 import { CreateTicketDto } from './dto/create-ticket.dto';
+import { TicketDto } from './dto/ticket.dto';
 import { UpdateTicketDto } from './dto/update-ticket.dto';
 
 @Injectable()
 export class TicketService {
-  create(createTicketDto: CreateTicketDto) {
-    return 'This action adds a new ticket';
+  constructor(@Inject(PG_CONNECTION) private connection: Pool) {}
+
+  /* TODO: test implementation */
+  async create(createTicketDto: CreateTicketDto) {
+    const {
+      category,
+      description,
+      manufacturer,
+      model,
+      os,
+      os_version,
+      priority,
+      status,
+    } = createTicketDto;
+
+    /* Insert new ticket into db */
+    const query: QueryConfig = {
+      name: 'insertTicket',
+      text:
+        'INSERT INTO ticket (category, description, manufacturer, model, os, os_version, priority, status),  ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
+      values: [
+        category,
+        description,
+        manufacturer,
+        model,
+        os,
+        os_version,
+        priority,
+        status,
+      ],
+    };
+    /* Handle db errors */
+    try {
+      const res = await this.connection.query<TicketDto>(query);
+      /* If no db errors return Ticket object */
+      return res.rows[0];
+    } catch (error) {
+      /* If db error return response with error and status code 422 */
+      const err = {
+        status: HttpStatus.UNPROCESSABLE_ENTITY,
+        error: error,
+        message: { name: query.name, text: query.text, values: query.values },
+      };
+      throw new HttpException(err, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
   }
 
-  findAll() {
-    return `This action returns all ticket`;
+  /* TODO: test implementation */
+  async findAll() {
+    try {
+      const query = 'SELECT * FROM ticket';
+      const queryRes = await this.connection.query<TicketDto>(query);
+
+      /* If no tickets */
+      if (queryRes.rows.length === 0) {
+        return [];
+      }
+
+      return [...queryRes.rows];
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.UNPROCESSABLE_ENTITY,
+          error: error,
+        },
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} ticket`;
+  /* TODO: test implementation */
+  async findOne(ticket_id: number) {
+    try {
+      const query = 'SELECT * FROM ticket WHERE ticket_id = $1';
+      const queryRes = await this.connection.query<TicketDto, number[]>(query, [
+        ticket_id,
+      ]);
+
+      /* If no customer found */
+      if (queryRes.rows.length === 0) {
+        Logger.verbose(
+          {
+            res: {
+              body: {},
+            },
+          },
+          'TicketService.findOne',
+          false,
+        );
+        return {};
+      }
+      Logger.verbose(
+        {
+          res: {
+            body: queryRes.rows[0],
+          },
+        },
+        'TicketService.findOne',
+        false,
+      );
+      return queryRes.rows[0];
+    } catch (error) {
+      const err = {
+        status: HttpStatus.UNPROCESSABLE_ENTITY,
+        error: error,
+      };
+      Logger.error({ message: err, context: 'TicketService.findone' });
+      throw new HttpException(err, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
   }
 
-  update(id: number, updateTicketDto: UpdateTicketDto) {
-    return `This action updates a #${id} ticket`;
-  }
+  /* TODO: test implementation */
+  async update(id: number, updateTicketDto: UpdateTicketDto) {
+    let keys = [];
+    let values = [];
 
-  remove(id: number) {
-    return `This action removes a #${id} ticket`;
+    /* unknown properties to be updated */
+    for (let key in updateTicketDto) {
+      if (!!updateTicketDto[key]) {
+        keys = [...keys, key];
+        values = [...values, updateTicketDto[key]];
+      }
+    }
+    /* keys variable defined by interface NOT user so avoids sql injection */
+    const columns = keys.map((val, idx) => `${val} = $${idx + 1}`).join(', ');
+    try {
+      const query = `UPDATE ticket SET ${columns} WHERE id = $${keys.length}`;
+      const queryRes = await this.connection.query(query, [...values, id]);
+      return queryRes.rows;
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.UNPROCESSABLE_ENTITY,
+          error: error,
+        },
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
   }
 }
