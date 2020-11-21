8605daf0d7ca6291f9c0f634b1bfa69250828de8
Alex <alxford45@gmail.com>
2020-11-20 18:00:11 -0600

    Logging updated; swagger route moved /docs -> /api/docs

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,8 +2,11 @@ import { NestApplicationOptions } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || 'localhost';
+
 async function bootstrap() {
   const appOptions: NestApplicationOptions = { cors: true, bodyParser: true };
   const app = await NestFactory.create(AppModule, appOptions);
@@ -12,14 +15,24 @@ async function bootstrap() {
     .setTitle('Support Ticket API')
     .setDescription('complete documentation for backend api')
     .setVersion('0.1.0')
-    .setBasePath('api')
+    .setExternalDoc(
+      'additional documentation',
+      `http://${HOST}:${PORT}/api/docs/more`,
+    )
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('/docs', app, document);
+  SwaggerModule.setup('/api/docs', app, document);
 
   // Starts listening for shutdown hooks emitted by heroku
   app.enableShutdownHooks();
 
-  await app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+  await app.listen(PORT, () => {
+    Logger.debug(`Now listening on port ${PORT}`, 'Main.bootstrap');
+    Logger.debug(
+      `View and test API Schema: http://${HOST}:${PORT}/api/docs`,
+      'Main.bootstrap',
+    );
+    Logger.debug(`Client served: http://${HOST}:${PORT}`, 'Main.bootstrap');
+  });
 }
 bootstrap();
