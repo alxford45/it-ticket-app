3094af2691d5b3a22e70bda1db9aab44ed3ee1bd
Alex <alxford45@gmail.com>
2020-11-20 21:04:29 -0600

    added function to control static or dynamic client rendering

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module, ModuleMetadata } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AppController } from './app.controller';
@@ -9,18 +9,43 @@ import { TicketWorkModule } from './ticket/work/ticketwork.module';
 import { TicketAssignModule } from './ticket/assign/ticketassign.module';
 import { CustomerModule } from './customer/customer.module';
 
+/**
+ *
+ * @param modules
+ * @returns Production Environment ? [ServeSataticModule, ...modules]
+ *                                 : [...modules]
+ *
+ * DEV Script: yarn start:dev | yarn start
+ * - Serve ONLY server at http://localhost:5000
+ * - Start client on a different terminal at http://localhost:3000
+ *
+ * PROD Script: yarn start:prod:
+ * - Serves both client and server on single terminal at http://${process.env.HOST}:5000
+ */
+const configImports = (modules: ModuleMetadata['imports']) => {
+  Logger.verbose(`Environment set to: ${process.env.NODE_ENV}`, 'ConfigModule');
+
+  const ClientModule = ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '../..', 'client/build'),
+    exclude: ['/api*'],
+  });
+
+  modules =
+    process.env.NODE_ENV === 'production'
+      ? [ClientModule, ...modules]
+      : [...modules];
+
+  return modules;
+};
+
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../..', 'client/build'),
-      exclude: ['/api*'],
-    }),
+  imports: configImports([
     TechModule,
     TicketModule,
     TicketWorkModule,
     TicketAssignModule,
     CustomerModule,
-  ],
+  ]),
   controllers: [AppController],
   providers: [AppService],
 })
