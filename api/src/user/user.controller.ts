94f0de90868b26cc1e5d1896e0b53323d9cdd030
Alex <alxford45@gmail.com>
2020-11-27 17:02:57 -0600

    added methods findAllAdmin, findAllStudents

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -8,23 +8,37 @@ import {
   Delete,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateCustomerDto } from './dto/create-user.dto';
+import { CreateUser } from './dto/create-user.dto';
+import { UserType } from './dto/user.dto';
 import { ApiTags } from '@nestjs/swagger';
 
-@ApiTags('customer')
-@Controller('/api/customer')
+@ApiTags('user')
+@Controller('/api/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   /* TODO: test implementation */
   @Post()
-  async create(@Body() createUserDto: CreateCustomerDto) {
+  async create(@Body() createUserDto: CreateUser) {
     return await this.userService.create(createUserDto);
   }
   /* TODO: test implementation */
   @Get()
   findAll() {
-    return this.userService.findAll();
+    return this.userService.findAll(UserType.USER);
   }
+
+  /* TODO: test implementation */
+  @Get('/admin')
+  findAllAdmin() {
+    return this.userService.findAll(UserType.ADMIN);
+  }
+
+  /* TODO: test implementation */
+  @Get('/student')
+  findAllStudents() {
+    return this.userService.findAll(UserType.STUDENT);
+  }
+
   /* TODO: test implementation */
   @Get(':id')
   findOne(@Param('lsu_id') lsu_id: number) {
