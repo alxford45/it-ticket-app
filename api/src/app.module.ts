92dd34b01db70fed39720b673baca166a004e495
Alex <alxford45@gmail.com>
2020-11-27 18:05:40 -0600

    flattened directory structure

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,8 +4,8 @@ import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TicketModule } from './ticket/ticket.module';
-import { TicketWorkModule } from './ticket/work/ticketwork.module';
-import { TicketAssignModule } from './ticket/assign/ticketassign.module';
+import { TicketWorkModule } from './work/ticketwork.module';
+import { TicketAssignModule } from './assign/ticketassign.module';
 import { UserModule } from './user/uer.module';
 
 /**
