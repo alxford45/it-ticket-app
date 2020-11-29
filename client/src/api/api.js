936668624ac65c334deda873efb23bffcc28ae96
Alex <alxford45@gmail.com>
2020-11-29 17:09:39 -0600

    heroku quick fix

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -5,8 +5,12 @@ If serve static dev -> host = localhost:3000
 If concurrent dev   -> host = localhost:5000
 If production       -> host = 'https://lsu-it-support-demo.herokuapp.com/'
 */
-const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || `localhost:${PORT}`
+// const PORT = process.env.PORT || 5000;
+// const HOST = process.env.HOST || `localhost:${PORT}`
+
+/* Heroku env vars not working :( */
+const LAZY = 'https://lsu-it-support-demo.herokuapp.com/api/'
 export default axios.create({
-  baseURL: `http://${HOST}/api/`,
+  // baseURL: `http://${HOST}/api/`,
+  baseURL: LAZY
 });
