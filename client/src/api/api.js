380862a4a55b8f4353fadf86676c2754d2301584
Alex <alxford45@gmail.com>
2020-11-29 16:21:35 -0600

    configured axios base url for dynamic environments

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+/* 
+If serve static dev -> host = localhost:3000
+If concurrent dev   -> host = localhost:5000
+If production       -> host = 'https://lsu-it-support-demo.herokuapp.com/'
+*/
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || `localhost:${PORT}`
 export default axios.create({
-  baseURL: `http://localhost:3000/api/`,
+  baseURL: `http://${HOST}/api/`,
 });
