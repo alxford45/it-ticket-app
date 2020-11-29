6ff2e8fd85ba0bf1f87079c61dc306224600ae55
Alex <alxford45@gmail.com>
2020-11-28 18:10:42 -0600

    updated dtos

--- api/src/assignment/dto/assignment.dto.ts
+++ api/src/assignment/dto/assignment.dto.ts
@@ -1,6 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+export enum AssignmentType {
+  'LSU_ID',
+  'TICKET_ID',
+}
 export class AssignmentDTO {
-  assignedby: string;
-  assignedto: string;
-  comment: string;
-  assigndate: Date;
+  @ApiProperty({
+    description: 'serial generated assignment id',
+    example: 1,
+  })
+  assignment_id: number;
+
+  @ApiProperty({
+    minLength: 9,
+    maxLength: 9,
+    description: '9-digit number starting with 89',
+    example: 897584512,
+  })
+  lsu_id: number;
+
+  @ApiProperty({
+    description: 'serial generated ticket id',
+    example: 1,
+  })
+  ticket_id: number;
+
+  @ApiProperty({
+    description: 'YYYY-MM-DD HH:MM:SS',
+    example: '2020-07-21 12:44:22',
+  })
+  assigned_date: string;
 }
