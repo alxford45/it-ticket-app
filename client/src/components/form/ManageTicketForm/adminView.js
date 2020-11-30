3ce46a4ea04f4907126c87b181a0c905597e07ed
Christian Dalton <cdalton713@gmail.com>
2020-11-30 10:05:27 -0600

    Revert "notes typo"
    This reverts commit d4848a1f0c2618a80b997e21a4046f1302bcbb5a.

--- client/src/components/form/ManageTicketForm/adminView.js
+++ client/src/components/form/ManageTicketForm/adminView.js
@@ -106,7 +106,7 @@ export const AdminView = (
               name={_.find(data, ["name", "notes"]).name}
               onChange={(e) => handleFormFieldChange(e, data, dispatch)}
               onBlur={(e) => handleFormFieldBlur(e, data, dispatch)}
-              value={_.find(data, ["name", "notes"]).value}
+              value={_.find(data, ["name", "description"]).value}
             />
           </EuiFormRow>
         </EuiFlexItem>
